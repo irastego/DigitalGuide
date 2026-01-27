e3f601891a347b29a5ff6933fc62b35a7c9a68e4
irastego <ivan.rastegorac@gmail.com>
2026-01-27 16:01:36 +0100

    Initial commit

---
+++ backend/src/main/java/com/example/digital_guide/DigitalGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.digital_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalGuideApplication.class, args);
+	}
+
+}
